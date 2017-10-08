52ae637ff1e4ab83dce0015d90ce803bfd159af1
kaconk17 <32590692+kaconk17@users.noreply.github.com>
2017-10-08 12:05:33 +0700

    fix soal8 print int hasil 0

--- Tugas/soal8.c
+++ Tugas/soal8.c
@@ -3,15 +3,17 @@
 //  Menerima masukan dengan format int dan char
 //  kemudian Menampilkannya kembali dengan format yang sesuai
 //  Created by kaconk on 10/7/17.
-//  
+//
 //
 #include <stdio.h>
 int main() {
-  int angka;
+  int angka, d;
   char karakter;
   printf("Masukkan angka!\n");
   scanf("%d",&angka);
   printf("Masukkan Huruf!\n");
   scanf("%s",&karakter);
-  printf("Angkanya adalah :%d dan Hurufnya adalah :%c\n", angka, karakter);
+  d= angka;
+  //printf("Angkanya adalah :%d dan Hurufnya adalah :%c\n", angka, karakter);
+  printf ("Angkanya %d dan Hurufnya %c\n",d, karakter);
 }
