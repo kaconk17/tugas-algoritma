8f44bf9796f9b6f0d7ab58b5c987031bb59a9e04
kaconk17 <32590692+kaconk17@users.noreply.github.com>
2017-12-31 17:50:55 +0700

    login fix

--- Tugas/login.c
+++ Tugas/login.c
@@ -8,26 +8,35 @@ struct id {
 struct id login;
 char user_login[50];
 char user_pass[50];
-int hasil_user, hasil_pass;
+int hasil_user, hasil_pass,i,stop=0;
 int *nilai;
 int cek_user();
+
+
 int main() {
+  do {
+  i =  cek_user();
+  if (i==0) {
+    printf("Selamat datang %s\n", login.user);
+    break;
+  } else {
+  printf("Login salah\n");
+  printf("Kesempatan anda tinggal %d kali\n", 2-stop );
+  stop++;
+  }
+} while(stop < 3);
+
+}
+
+
+int cek_user(){
+  int p;
   strcpy(login.user, "admin");
   strcpy(login.pass, "admin");
   printf("Masukkan user anda \n");
   gets(user_login);
   printf("Masukkan password anda \n");
   gets(user_pass);
-
-  if ( cek_user(user_login, user_pass)==0){
-      printf("Selamat datang\n");
-  }else {
-    printf("Login salah\n");
-  }
-  //return 0;
-}
-int cek_user(){
-  int p;
   hasil_user = strcmp(user_login,login.user);
   hasil_pass = strcmp(user_pass,login.pass);
   p=hasil_user + hasil_pass;
